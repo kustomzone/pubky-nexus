2d40b019d61d1e4e6d0f74d9b9f74950d17a96db
amir <amir.r.amirfatahi@gmail.com>
2024-09-27 00:23:16 -0400

    add image and video processing

--- src/events/handlers/file.rs
+++ src/events/handlers/file.rs
@@ -3,10 +3,7 @@ use std::error::Error;
 use axum::body::Bytes;
 use log::debug;
 use pubky::PubkyClient;
-use tokio::{
-    fs::{self, remove_file, File},
-    io::AsyncWriteExt,
-};
+use tokio::fs;
 
 use crate::{
     models::{
@@ -18,7 +15,7 @@ use crate::{
         traits::Collection,
         user::PubkyId,
     },
-    Config,
+    static_processor::store::{remove_blob, store_blob},
 };
 
 pub async fn put(
@@ -75,45 +72,6 @@ async fn ingest(
             main: main_static_path,
         },
     })
-}
-
-async fn store_blob(
-    name: String,
-    path: String,
-    blob: &Bytes,
-) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let storage_path = Config::from_env().file_path;
-    let full_path = format!("{}/{}", storage_path, path);
-
-    debug!("store blob in full_path: {}", full_path);
-
-    let path_exists = match fs::metadata(full_path.as_str()).await {
-        Err(_) => false,
-        Ok(metadata) => metadata.is_dir(),
-    };
-
-    debug!("path exists: {}", path_exists);
-
-    if !path_exists {
-        fs::create_dir_all(full_path.as_str()).await?;
-    }
-
-    let file_path = format!("{}/{}", full_path, name);
-    let mut static_file = File::create_new(file_path).await?;
-    static_file.write_all(blob).await?;
-
-    Ok(())
-}
-
-async fn remove_blob(
-    name: String,
-    path: String,
-) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let storage_path = Config::from_env().file_path;
-    let file_path = format!("{}/{}/{}", storage_path, path, name);
-
-    remove_file(file_path).await?;
-    Ok(())
 }
 
 pub async fn del(user_id: &PubkyId, file_id: String) -> Result<(), Box<dyn Error + Sync + Send>> {
